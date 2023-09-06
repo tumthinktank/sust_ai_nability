6e18ccd673dca5a1112fd5b9f5d1b7c8b00f5fe5
crosssenses <lari_wu@gmx.de>
2023-09-06 15:19:23 +0200

    debug env

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,8 @@ import Logo from "../assets/Logo.svg"
 console.log("date: ", new Date(2023, 8, 9) < new Date());
 console.log("mode: ", process.env.GATSBY_MODE)
 
-const ENV = new Date(2023, 8, 9) < new Date() ? process.env.GATSBY_MODE : "LIVE";
+// const ENV = new Date(2023, 8, 9) < new Date() ? process.env.GATSBY_MODE : "LIVE";
+const ENV = "LIVE"
 
 const LayoutWrapper = styled.div`
   display: flex;
